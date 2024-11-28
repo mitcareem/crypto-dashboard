fec18964b4b2c1ea1a206dee860e67f86bb8d467
mitcaree <mitcareem@gmail.com>
2024-11-28 11:27:31 +0530

    fix: error handler

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
+import { RefreshCw } from "lucide-react";
 
 const ErrorMessage: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <div className="flex items-center justify-center h-screen bg-background">
+    <div className="flex gap-[1rem] items-center justify-center h-screen bg-background">
       <p className="font-size-large font-semibold text-warning animate-pulse">
-       {"Some error occurred :)"}
+        {"Some error occurred :)"}
       </p>
+      <Button
+        onClick={() => navigate("/")}
+        className="flex flex-row gap-[1rem]"
+      >
+        refresh <RefreshCw size={14} />
+      </Button>
     </div>
   );
 };
